7db5c5a6daa0a407279e8ffb5d565e4d677b7579
vtphatt2 <thinhphat544@gmail.com>
2023-11-07 10:50:44 +0700

    src/game.cpp - Add menu

--- src/game.cpp
+++ src/game.cpp
@@ -4,20 +4,32 @@ Game::Game() : widthWindow(1430),  heightWindow(990) {
     level = 1;
     highScore = 0;
     score = 0;
+    currentState = GameState::Entry;
+    window.create(sf::VideoMode(widthWindow, heightWindow), "ROAD CROSSING");
 }
 
 void Game::run() {
-    sf::RenderWindow window(sf::VideoMode(widthWindow, heightWindow), "ROAD CROSSING");
+    while (window.isOpen()) {
+        if (currentState == GameState::Entry) {
+            runEntry();
+        }
+        else if (currentState == GameState::Menu) {
+            runMenu();
+        }
+    }
+}
 
+void Game::runEntry() {
     sf::Texture backgroundTexture;
     backgroundTexture.loadFromFile("../resource/Splash_Screen.png");
     sf::Sprite backgroundImage(backgroundTexture);
-    backgroundImage.setPosition(0, -50);
+    backgroundImage.setPosition(0, -70);
 
-    sf::Texture playbuttonTexture;
-    playbuttonTexture.loadFromFile("../resource/Play_Button.png");
-    sf::Sprite playbuttonImage(playbuttonTexture);
-    playbuttonImage.setPosition(673, 636);
+    sf::Texture playButtonTexture;
+    playButtonTexture.loadFromFile("../resource/Play_Button.png");
+    sf::Sprite playButtonImage(playButtonTexture);
+    playButtonImage.setPosition(673, 636);
+    playButtonImage.setColor(sf::Color(255, 255, 255, 220));
     sf::IntRect playButtonRect(673, 636, 84, 84);
 
     while (window.isOpen()) {
@@ -28,6 +40,17 @@ void Game::run() {
                     window.close();
                     break;
                 
+                case sf::Event::MouseButtonPressed :
+                    if (event.mouseButton.button == sf::Mouse::Left) {
+                        sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+
+                        if (playButtonRect.contains(mousePosition)) {
+                            currentState = GameState::Menu;
+                            return ;
+                        }
+                    }
+                    break;
+    
                 default :
                     break;
             }
@@ -35,15 +58,40 @@ void Game::run() {
 
         sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
         if (playButtonRect.contains(mousePosition)) {
-            playbuttonImage.setColor(sf::Color::Red);
+            playButtonImage.setColor(sf::Color(255, 255, 255, 255));
         } 
         else {
-            playbuttonImage.setColor(sf::Color::White);
+            playButtonImage.setColor(sf::Color(255, 255, 255, 200));
         }
 
         window.clear();
         window.draw(backgroundImage);
-        window.draw(playbuttonImage);
+        window.draw(playButtonImage);
         window.display();
+    }
+}
+
+void Game::runMenu() {
+    sf::Texture backgroundTexture;
+    backgroundTexture.loadFromFile("../resource/Menu.png");
+    sf::Sprite backgroundImage(backgroundTexture);
+    backgroundImage.setPosition(0, -125);
+
+        while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                case sf::Event::Closed :
+                    window.close();
+                    break;
+    
+                default :
+                    break;
+            }
+        }
+
+    window.clear();
+    window.draw(backgroundImage);
+    window.display();
     }
 }
--- src/header/game.hpp
+++ src/header/game.hpp
@@ -1,15 +1,27 @@
-#include "SFML/Graphics.hpp"
 #include <iostream>
+#include "SFML/Graphics.hpp"
+
+enum class GameState {
+    Entry,
+    Menu,
+};
 
 class Game {
     public :
         Game();
         void run();
 
+        void runEntry();
+        void runMenu();
+
     private :
         int level;
         int score;
         int highScore;
+
         const int widthWindow;
         const int heightWindow;
+        sf::RenderWindow window;
+
+        GameState currentState;
 };

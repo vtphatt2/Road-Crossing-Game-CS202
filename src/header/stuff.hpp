87bf6a64a5a5c7b782cbcb4062c1507b01ee09cf
duckzuybidan <ducduy19012310@gmail.com>
2023-11-14 00:14:16 +0700

    .

--- src/header/stuff.hpp
+++ src/header/stuff.hpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿#pragma once
 #include"SFML/Graphics.hpp"
 //Edit here!
@@ -64,6 +65,60 @@
 //UFO
 enum class UFOColor {
 	blue, brown, pink, green
+=======
+﻿#ifndef OBSTACLES_HPP
+#define OBSTACLES_HPP
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+// Edit here!
+#define blueUFOPath ""
+#define brownUFOPath ""
+#define pinkUFOPath ""
+#define greenUFOPath ""
+
+#define leftCoinPath ""
+#define rightCoinPath ""
+
+#define movingAnt1Path ""
+#define movingAnt2Path ""
+#define inRestAntPath ""
+
+#define bird1Path ""
+#define bird2Path ""
+
+#define movingBat1Path ""
+#define movingBat2Path ""
+#define inRestBatPath ""
+
+#define movingWormPath ""
+#define inRestWormPath ""
+
+#define movingMonster1Path ""
+#define movingMonster2Path ""
+#define inRestMonsterPath ""
+
+#define slime1Path ""
+#define slime2Path ""
+#define slime3Path ""
+
+#define movingSnailPath ""
+#define inRestSnailPath ""
+
+#define movingGhost1Path ""
+#define movingGhost2Path ""
+#define inRestGhostPath ""
+
+#define defaultSpeed 0.01
+
+// UFO
+enum class UFOColor
+{
+	blue,
+	brown,
+	pink,
+	green
+>>>>>>> Stashed changes
 };
 class UFO : public sf::Drawable
 {
@@ -71,7 +126,11 @@ public:
 	UFO();
 	UFO(UFOColor color);
 	sf::Vector2f getPosition();
+<<<<<<< Updated upstream
 	void setPosition(const sf::Vector2f& position);
+=======
+	void setPosition(const sf::Vector2f &position);
+>>>>>>> Stashed changes
 	void setPosition(float x, float y);
 	sf::FloatRect getGlobalBounds();
 	float getSpeed();
@@ -79,19 +138,35 @@ public:
 	void run();
 	void stop();
 	void resume();
+<<<<<<< Updated upstream
 private:
 	sf::Texture texture;
 	sf::Sprite sprite;
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+=======
+
+private:
+	sf::Texture texture;
+	sf::Sprite sprite;
+	void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+>>>>>>> Stashed changes
 	float speed = defaultSpeed;
 	int numOfMoves = 0;
 	bool isRunning = true;
 	int limit = 20 / speed;
 };
 
+<<<<<<< Updated upstream
 //coin
 enum class CoinType {
 	left, right
+=======
+// coin
+enum class CoinType
+{
+	left,
+	right
+>>>>>>> Stashed changes
 };
 class Coin : public sf::Drawable
 {
@@ -99,6 +174,7 @@ public:
 	Coin();
 	Coin(CoinType type);
 	sf::Vector2f getPosition();
+<<<<<<< Updated upstream
 	void setPosition(const sf::Vector2f& position);
 	void setPosition(float x, float y);
 	sf::FloatRect getGlobalBounds();
@@ -110,12 +186,30 @@ private:
 };
 
 //Ant
+=======
+	void setPosition(const sf::Vector2f &position);
+	void setPosition(float x, float y);
+	sf::FloatRect getGlobalBounds();
+	void run();
+
+private:
+	sf::Texture texture;
+	sf::Sprite sprite;
+	void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+};
+
+// Ant
+>>>>>>> Stashed changes
 class Ant : public sf::Drawable
 {
 public:
 	Ant();
 	sf::Vector2f getPosition();
+<<<<<<< Updated upstream
 	void setPosition(const sf::Vector2f& position);
+=======
+	void setPosition(const sf::Vector2f &position);
+>>>>>>> Stashed changes
 	void setPosition(float x, float y);
 	sf::FloatRect getGlobalBounds();
 	float getSpeed();
@@ -123,23 +217,39 @@ public:
 	void run();
 	void stop();
 	void resume();
+<<<<<<< Updated upstream
 private:
 	sf::Texture texture[3];
 	sf::Sprite sprite;
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+=======
+
+private:
+	sf::Texture texture[3];
+	sf::Sprite sprite;
+	void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+>>>>>>> Stashed changes
 	float speed = defaultSpeed;
 	int numOfMoves = 0;
 	bool isRunning = true;
 	int limit = 20 / speed;
 };
 
+<<<<<<< Updated upstream
 //Bird
+=======
+// Bird
+>>>>>>> Stashed changes
 class Bird : public sf::Drawable
 {
 public:
 	Bird();
 	sf::Vector2f getPosition();
+<<<<<<< Updated upstream
 	void setPosition(const sf::Vector2f& position);
+=======
+	void setPosition(const sf::Vector2f &position);
+>>>>>>> Stashed changes
 	void setPosition(float x, float y);
 	sf::FloatRect getGlobalBounds();
 	float getSpeed();
@@ -147,23 +257,39 @@ public:
 	void run();
 	void stop();
 	void resume();
+<<<<<<< Updated upstream
 private:
 	sf::Texture texture[2];
 	sf::Sprite sprite;
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+=======
+
+private:
+	sf::Texture texture[2];
+	sf::Sprite sprite;
+	void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+>>>>>>> Stashed changes
 	float speed = defaultSpeed;
 	int numOfMoves = 0;
 	bool isRunning = true;
 	int limit = 20 / speed;
 };
 
+<<<<<<< Updated upstream
 //Bat
+=======
+// Bat
+>>>>>>> Stashed changes
 class Bat : public sf::Drawable
 {
 public:
 	Bat();
 	sf::Vector2f getPosition();
+<<<<<<< Updated upstream
 	void setPosition(const sf::Vector2f& position);
+=======
+	void setPosition(const sf::Vector2f &position);
+>>>>>>> Stashed changes
 	void setPosition(float x, float y);
 	sf::FloatRect getGlobalBounds();
 	float getSpeed();
@@ -171,23 +297,39 @@ public:
 	void run();
 	void stop();
 	void resume();
+<<<<<<< Updated upstream
 private:
 	sf::Texture texture[3];
 	sf::Sprite sprite;
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+=======
+
+private:
+	sf::Texture texture[3];
+	sf::Sprite sprite;
+	void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+>>>>>>> Stashed changes
 	float speed = defaultSpeed;
 	int numOfMoves = 0;
 	bool isRunning = true;
 	int limit = 20 / speed;
 };
 
+<<<<<<< Updated upstream
 //Worm
+=======
+// Worm
+>>>>>>> Stashed changes
 class Worm : public sf::Drawable
 {
 public:
 	Worm();
 	sf::Vector2f getPosition();
+<<<<<<< Updated upstream
 	void setPosition(const sf::Vector2f& position);
+=======
+	void setPosition(const sf::Vector2f &position);
+>>>>>>> Stashed changes
 	void setPosition(float x, float y);
 	sf::FloatRect getGlobalBounds();
 	float getSpeed();
@@ -195,23 +337,39 @@ public:
 	void run();
 	void stop();
 	void resume();
+<<<<<<< Updated upstream
 private:
 	sf::Texture texture[2];
 	sf::Sprite sprite;
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+=======
+
+private:
+	sf::Texture texture[2];
+	sf::Sprite sprite;
+	void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+>>>>>>> Stashed changes
 	float speed = defaultSpeed;
 	int numOfMoves = 0;
 	bool isRunning = true;
 	int limit = 20 / speed;
 };
 
+<<<<<<< Updated upstream
 //Monster
+=======
+// Monster
+>>>>>>> Stashed changes
 class Monster : public sf::Drawable
 {
 public:
 	Monster();
 	sf::Vector2f getPosition();
+<<<<<<< Updated upstream
 	void setPosition(const sf::Vector2f& position);
+=======
+	void setPosition(const sf::Vector2f &position);
+>>>>>>> Stashed changes
 	void setPosition(float x, float y);
 	sf::FloatRect getGlobalBounds();
 	float getSpeed();
@@ -219,24 +377,42 @@ public:
 	void run();
 	void stop();
 	void resume();
+<<<<<<< Updated upstream
 private:
 	sf::Texture texture[3];
 	sf::Sprite sprite;
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+=======
+
+private:
+	sf::Texture texture[3];
+	sf::Sprite sprite;
+	void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+>>>>>>> Stashed changes
 	float speed = defaultSpeed;
 	int numOfMoves = 0;
 	bool isRunning = true;
 	int limit = 20 / speed;
+<<<<<<< Updated upstream
 	bool toTheRight = true;
 };
 
 //Slime
+=======
+};
+
+// Slime
+>>>>>>> Stashed changes
 class Slime : public sf::Drawable
 {
 public:
 	Slime();
 	sf::Vector2f getPosition();
+<<<<<<< Updated upstream
 	void setPosition(const sf::Vector2f& position);
+=======
+	void setPosition(const sf::Vector2f &position);
+>>>>>>> Stashed changes
 	void setPosition(float x, float y);
 	sf::FloatRect getGlobalBounds();
 	float getSpeed();
@@ -244,10 +420,18 @@ public:
 	void run();
 	void stop();
 	void resume();
+<<<<<<< Updated upstream
 private:
 	sf::Texture texture[3];
 	sf::Sprite sprite;
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+=======
+
+private:
+	sf::Texture texture[3];
+	sf::Sprite sprite;
+	void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+>>>>>>> Stashed changes
 	float speed = defaultSpeed;
 	int numOfMoves = 0;
 	bool isRunning = true;
@@ -255,13 +439,21 @@ private:
 	bool hidden;
 };
 
+<<<<<<< Updated upstream
 //Snail
+=======
+// Snail
+>>>>>>> Stashed changes
 class Snail : public sf::Drawable
 {
 public:
 	Snail();
 	sf::Vector2f getPosition();
+<<<<<<< Updated upstream
 	void setPosition(const sf::Vector2f& position);
+=======
+	void setPosition(const sf::Vector2f &position);
+>>>>>>> Stashed changes
 	void setPosition(float x, float y);
 	sf::FloatRect getGlobalBounds();
 	float getSpeed();
@@ -269,23 +461,39 @@ public:
 	void run();
 	void stop();
 	void resume();
+<<<<<<< Updated upstream
 private:
 	sf::Texture texture[2];
 	sf::Sprite sprite;
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+=======
+
+private:
+	sf::Texture texture[2];
+	sf::Sprite sprite;
+	void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+>>>>>>> Stashed changes
 	float speed = defaultSpeed;
 	int numOfMoves = 0;
 	bool isRunning = true;
 	int limit = 20 / speed;
 };
 
+<<<<<<< Updated upstream
 //Ghost
+=======
+// Ghost
+>>>>>>> Stashed changes
 class Ghost : public sf::Drawable
 {
 public:
 	Ghost();
 	sf::Vector2f getPosition();
+<<<<<<< Updated upstream
 	void setPosition(const sf::Vector2f& position);
+=======
+	void setPosition(const sf::Vector2f &position);
+>>>>>>> Stashed changes
 	void setPosition(float x, float y);
 	sf::FloatRect getGlobalBounds();
 	float getSpeed();
@@ -293,16 +501,25 @@ public:
 	void run();
 	void stop();
 	void resume();
+<<<<<<< Updated upstream
 private:
 	sf::Texture texture[3];
 	sf::Sprite sprite;
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+=======
+
+private:
+	sf::Texture texture[3];
+	sf::Sprite sprite;
+	void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+>>>>>>> Stashed changes
 	float speed = defaultSpeed;
 	int numOfMoves = 0;
 	bool isRunning = true;
 	int limit = 20 / speed;
 };
 
+<<<<<<< Updated upstream
 //Frog
 class Frog : public sf::Drawable
 {
@@ -431,3 +648,6 @@ private:
 	bool isJumping = 0;
 };
 
+=======
+#endif
+>>>>>>> Stashed changes
